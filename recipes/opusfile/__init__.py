dfffeb5f9237468cc6e0a79352b8da4558d684d9
Kyle Perry <kyleperry@Mac.lan>
2026-07-19 10:47:30 -1000

    Remove rich debug import from opusfile recipe

--- recipes/opusfile/__init__.py
+++ recipes/opusfile/__init__.py
@@ -33,9 +33,9 @@ class OpusFileRecipe(Recipe):
             # env['CFLAGS'] += openssl_recipe.include_flags(arch)
             # env['LDFLAGS'] += openssl_recipe.link_dirs_flags(arch)
             # env['LIBS'] = openssl_recipe.link_libs_flags()
+                     
+
             
-            from rich.pretty import pprint
-            pprint(env)
             time.sleep(5)
 
             configure = sh.Command('./configure')
