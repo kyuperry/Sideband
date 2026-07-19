32683a3c4d51b86bb1d71b415226e97748d2793e
Kyle Perry <kyleperry@Mac.lan>
2026-07-19 10:06:18 -1000

    Make LXST Android recipe portable

--- recipes/lxst/__init__.py
+++ recipes/lxst/__init__.py
@@ -1,33 +1,43 @@
-"""
-Android Bluetooth Low Energy
-"""
+# Android Bluetooth Low Energy
+# -*- coding: utf-8 -*-
+
 from pythonforandroid.recipe import PythonRecipe
 from pythonforandroid.toolchain import current_directory, info, shprint
 import sh
-from os.path import join
+from os.path import abspath, join
 
 
 class LXSTRecipe(PythonRecipe):
-    name = 'lxst_recipe'
-    depends = ['python3', 'setuptools', 'android', "cffi"]
+    name = "lxst_recipe"
+    depends = ["python3", "setuptools", "android", "cffi"]
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
 
     def prepare_build_dir(self, arch):
         build_dir = self.get_build_dir(arch)
         assert build_dir.endswith(self.name)
-        shprint(sh.rm, '-rf', build_dir)
+        shprint(sh.rm, "-rf", build_dir)
         shprint(sh.mkdir, build_dir)
 
-        srcs = ('/home/markqvist/Information/Source/LXST/LXST', '/home/markqvist/Information/Source/LXST/setup.py', '/home/markqvist/Information/Source/LXST/README.md')
+        # GitHub Actions checks LXST out beside the Sideband directory.
+        # From recipes/lxst, move up to the Actions workspace and use LXST.
+        lxst_root = abspath(
+            join(self.get_recipe_dir(), "..", "..", "..", "LXST")
+        )
+
+        srcs = (
+            join(lxst_root, "LXST"),
+            join(lxst_root, "setup.py"),
+            join(lxst_root, "README.md"),
+        )
 
         for filename in srcs:
-            print(f"Copy {join(self.get_recipe_dir(), filename)} to {build_dir}")
-            shprint(sh.cp, '-a', join(self.get_recipe_dir(), filename),
-                    build_dir)
+            print(f"Copy {filename} to {build_dir}")
+            shprint(sh.cp, "-a", filename, build_dir)
 
     def postbuild_arch(self, arch):
         super(LXSTRecipe, self).postbuild_arch(arch)
         info("LXST native build completed")
+
 
 recipe = LXSTRecipe()
